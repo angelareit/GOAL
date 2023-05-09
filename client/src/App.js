0fee032408b17c6218b58153ba32ab5652c461ba
KovaKreative <eric.kovalevskyy@gmail.com>
2023-05-09 19:57:32 -0400

    Add placeholder sockets functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { io } from 'socket.io-client';
+import { useEffect } from 'react';
+
+const socket = io();
+
+useEffect(() => {
+  //https://socket.io/docs/v3/emit-cheatsheet/
+  // socket.on('MESSAGE', payload =>  )
+
+  return () => {
+    //socket.off('MESSAGE');
+  }
+}, []);
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -5,6 +5,11 @@ const morgan = require('morgan');
 
 const app = express();
 
+http = require('http');
+server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 //Add any middleware here
 
 app.use(morgan('dev'));
@@ -13,4 +18,10 @@ app.get('/', (req, res) => {
   return res.json({ greetings: "Universe" });
 });
 
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  // https://socket.io/docs/v3/emit-cheatsheet/
+});
+
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));

cbdec12065e6376aab64d076dd9cc0e55235ac24
KovaKreative <eric.kovalevskyy@gmail.com>
2023-05-12 13:31:22 -0400

    Add test websockets events

--- documents/PLANNING.md
+++ documents/PLANNING.md
@@ -5,3 +5,5 @@
 [Initial planning - Google Docs](https://docs.google.com/document/d/1zWmRfQrMQUnRqH_tC_rjPXO354E1I5ynB7ZUfTEU-tk/edit#heading=h.lmcyaoy7avrk)
 
 [Wireframes - Figma](https://www.figma.com/file/tqlbnGSMrg1nO4zRYH6QFl/Untitled?type=design&node-id=1%3A2&t=acFm72lw4e79m5AB-1)
+
+[Plannint - Trello](https://trello.com/b/spTEHTF9/lhl-final-productivity-app)
--- server/server.js
+++ server/server.js
@@ -18,10 +18,38 @@ app.get('/', (req, res) => {
   return res.json({ greetings: "Universe" });
 });
 
-io.on('connection', socket => {
-  console.log('a user connected');
+const users = [];
+const connection = [];
 
+io.on('connection', socket => {
+  const user = { user: socket.handshake.auth.user, buddy: socket.handshake.auth.buddy, id: socket.id };
+  if(!users.includes(user)) {
+    users.push(user);
+    connection.push(user);
+  }
+
+  if(connection.length > 2) {
+    connection.shift();
+  }
+
+  if(connection.length === 2) {
+    // if(connection[0].user === connection[1].buddy){
+    if(true){
+      console.log(connection[0].user, connection[1].user);
+      socket.to(connection[0].id).emit('BUDDY_ONLINE', true);
+      socket.to(connection[1].id).emit('BUDDY_ONLINE', true);
+    }
+  }
+
+  console.log("Connection:", connection);
+
+
+  socket.on('MESSAGE_SEND', payload => {
+    console.log(payload);
+    socket.broadcast.emit('MESSAGE_RECEIVE', payload);
+  })
   // https://socket.io/docs/v3/emit-cheatsheet/
+  
 });
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));

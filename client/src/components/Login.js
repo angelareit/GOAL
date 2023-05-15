ecf6a6bba7e79ee64a83187299df767ad12a8008
Xize Wu <wuxize1995@gmail.com>
2023-05-15 18:10:25 -0400

    add search page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,18 @@
 import React, { useState } from 'react';
+import { createStore } from 'redux';
+
+// function counterReducer(state = { value: 0 }, action) {
+//   switch (action.type) {
+//     case 'counter/incremented':
+//       return { value: state.value + 1 }
+//     case 'counter/decremented':
+//       return { value: state.value - 1 }
+//     default:
+//       return state
+//   }
+// }
+
+
 
 const Login = (props) => {
   const [users, setUsers] = useState();

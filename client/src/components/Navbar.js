5bc40ed67c61b8f3c6af0d2396d7c0a431e7a2ac
angelareit <anreit01@gmail.com>
2023-05-18 18:41:54 -0400

    nav bar styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import './Navbar.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon, solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { useSelector, useDispatch } from 'react-redux'
 import { showGoalListPanel, showBuddyPanel, showNotificationPanel } from '../features/viewManagerSlice';
 
 
 export default function Navbar(props) {
   const dispatch = useDispatch()
+  const viewState = useSelector((state) => state.viewManager)
+
   return (
     <nav className='nav'>
       <div>
-        {props?.username && <button onClick={() => dispatch(showGoalListPanel())}>Goal List</button>}
+        {props?.username && 
+          <FontAwesomeIcon 
+            className={`iconbtn`}
+            onClick={() => dispatch(showGoalListPanel())}
+            icon={icon({ name: "bars-staggered" })} 
+          />
+        }
         <h1>Project X</h1>
       </div>
       <div>
         {props?.username &&
           <>
-            <h2>{props.username}</h2>
-            <button onClick={props.onLogout}>Log Out</button>
-            <button onClick={() => dispatch(showBuddyPanel())}>Buddy</button>
-            <button onClick={() => dispatch(showNotificationPanel())}>Notifications</button>
+            <div className='dropdown'>
+              <div className='dropbtn'><h3>{props.username}</h3><FontAwesomeIcon icon={solid("caret-down")} /></div>
+              <div class="dropdown-content">
+                <button onClick={() => { }}>Account Settings</button>
+                <button onClick={props.onLogout}>Log Out</button>
+              </div>
+            </div>
+            <FontAwesomeIcon
+              className={`iconbtn-circle ${viewState.rightSideBar.currentView === 'buddy' && viewState.rightSideBar.visibility ? 'active' : undefined}`}
+              onClick={() => dispatch(showBuddyPanel())}
+              icon={solid("user-group")} />
+            <FontAwesomeIcon
+              className={`iconbtn-circle ${viewState.rightSideBar.currentView === 'notifications' && viewState.rightSideBar.visibility ? 'active' : undefined}`}
+              onClick={() => dispatch(showNotificationPanel())}
+              icon={solid("bell")} />
+
           </>
         }
       </div>

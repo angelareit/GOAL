7e6d5729e9718e1c9319046ac21008c10731dc88
Xize Wu <wuxize1995@gmail.com>
2023-05-19 17:59:59 -0400

    refactor to hide search view when buddy exists

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+
 import SearchBar from "./SearchBar";
 import NoBuddy from "./NoBuddy";
 import IncomingRequests from "./IncomingRequests";
 export default function Search(props) {
-  return (
-    <span>
-    <NoBuddy/>
-    <SearchBar/>
-    {/* {requestArray.length > 0 && <IncomingRequests/>} */}
-    <IncomingRequests/>
-    </span>
+ //see if the user has a buddy
+ //if buddyState.id is trusy, do not render incoming requests
+ //if buddyState.id is falsy, render incoming requests
+ const buddyState = useSelector(state => state.session.buddy);
+console.log('This is buddystate', buddyState)
+
+ return (
+    buddyState.id ? (<></>):(
+      <span>
+      <NoBuddy/>
+      <SearchBar/>
+      <IncomingRequests/>
+      </span>
+    )
   );
 }

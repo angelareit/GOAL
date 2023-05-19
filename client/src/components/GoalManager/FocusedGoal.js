fd7dafb552324675a91994a7d213de52e2f7a964
KovaKreative <eric.kovalevskyy@gmail.com>
2023-05-19 14:45:44 -0400

    Change focused goal title to be h1

--- client/src/components/GoalManager/FocusedGoal.js
+++ client/src/components/GoalManager/FocusedGoal.js
@@ -11,7 +11,7 @@ export default function FocusedGoal(props) {
 
   return (
     <div>
-      <h3>{data.title}</h3>
+      <h1>{data.title}</h1>
       <p>{data.note}</p>
       {data.due_date && <p>Deadline: {new Date(subGoal.due_date).toLocaleDateString('en-CA')}</p>}
       {data.category_id && <p>Category: {category}</p>}

1a4f6ed53da59da4417d06579ee73ce7fb4135f3
Xize Wu <wuxize1995@gmail.com>
2023-05-21 15:56:23 -0400

    fix name of login button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -48,7 +48,7 @@ const Login = (props) => {
 
       <div className="footer">
         <button type="submit" className="btn">
-          Create
+          Login
         </button>
       </div>
     </form>

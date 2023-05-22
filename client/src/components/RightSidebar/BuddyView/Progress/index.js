f9c308d49020001e5548e44d066bca0584a47aaa
angelareit <anreit01@gmail.com>
2023-05-22 18:25:58 -0400

    added message when buddy has no goals

--- client/src/components/RightSidebar/BuddyView/Progress/index.js
+++ client/src/components/RightSidebar/BuddyView/Progress/index.js
@@ -11,24 +11,16 @@ export default function Progress(props) {
   const buddyProgressState = useSelector((state) => state.session.buddyProgress)
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    console.log(buddyProgressState);
-  }, []);
-
 
   let progressBars = buddyProgressState.goalCounts.map(mainGoal => {
     let value = (mainGoal.completed_count / mainGoal.total_count) * 100;
-    console.log('MainGoal:', mainGoal.main_goal_id, value);
     return <ProgressCard title={mainGoal.main_goal_title} barValue={value} barMax={100} recentHistory={buddyProgressState.subGoalHistory[mainGoal.main_goal_id]}/>
   });
-
   
-
-
-
+  console.log('PROGRESS HERESS', typeof progressBars);
   return (
     <div className="progress">
-      {progressBars}
+      {Object.keys(progressBars ).length > 0 ? progressBars: <h3>Buddy currently has no goals.</h3>  }
     </div>
   )
 }
